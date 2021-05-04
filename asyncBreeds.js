f73587794ec04a08397d6ddae9e82d57c1a11897
Logan Thomas <logannormanthomas@protonmail.com>
2021-05-04 21:01:36 +0000

    finished cats with tests

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = (breed, callback) => {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
+  });
+};
+
+module.exports = breedDetailsFromFile;
